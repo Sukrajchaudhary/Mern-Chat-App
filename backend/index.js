cebbb834e137c80fb8ce642c729258268d80a3aa
Sukrajchaudhary <sukrajchaudhary90@gmail.com>
2024-08-04 10:35:19 +0545

    last

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,8 @@ const authRouter = require("./router/Auth.router");
 const messageRouter=require("./router/Message.router");
 const userRouter= require("./router/Users.router")
 const { app, server } = require("./socket/socket");
+const path=require("path");
+const __dir=path.resolve();
 connectToDb();
 app.use(
   cors({
@@ -24,6 +26,10 @@ app.use(cookieParser());
 app.use("/api/", authRouter.router);
 app.use("/api",messageRouter.router);
 app.use("/api",userRouter.router);
+app.use(express.static(path.join(__dir,"frontend/dist")));
+app.get("*",(req,res)=>{
+  res.sendFile(path.join(__dir,"frontend","dist","index.html"))
+})
 const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`App is Running ${PORT}`);
